d621fdef321fe8e475a3e9abab5aa901c301ef0e
Vinícios Costa <dev.vinicios@gmail.com>
2024-05-19 12:49:52 -0300

    style: add MUI custom theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -12,6 +12,30 @@ const theme = createTheme({
   typography: {
     fontFamily: roboto.style.fontFamily,
   },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 640,
+      md: 768,
+      lg: 1024,
+      xl: 1280,
+    },
+  },
+  palette: {
+    primary: {
+      "900": "#0d0714",
+      "800": "#15101c",
+      "700": "#1d1825",
+    },
+    secondary: {
+      main: "#78cfb0",
+      "500": "#9e78cf",
+      "300": "#ab89d6",
+    },
+    text: {
+      primary: "#777777",
+    },
+  },
 });
 
 export default theme;
