018e10d3d0df2cf26fbcaa04dc4f96f1e0c695d6
Vinícios Costa <dev.vinicios@gmail.com>
2024-05-20 20:41:15 -0300

    feat: created basic nav component

---
+++ src/components/nav.tsx
@@ -0,0 +1,20 @@
+import { Box, Button } from "@mui/material";
+import Link from "next/link";
+
+function Nav() {
+  return (
+    <Box component="nav" marginY="5rem">
+      <Link href={""}>
+        <Button color="secondary">To-Do</Button>
+      </Link>
+      <Link href={"/tasks/done"}>
+        <Button color="secondary">Done</Button>
+      </Link>
+      <Link href={"/tasks/deleted"}>
+        <Button color="secondary">Deleted</Button>
+      </Link>
+    </Box>
+  );
+}
+
+export default Nav;
